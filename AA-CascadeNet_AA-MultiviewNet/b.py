8020a5e899d296d554f2b731f5f9879cb0292d65
l.d.goene@students.uu.nl <lucas.goene@gmail.com>
2024-04-29 08:16:11 +0200

    Update

--- AA-CascadeNet_AA-MultiviewNet/b.py
+++ AA-CascadeNet_AA-MultiviewNet/b.py
@@ -1,8 +1,8 @@
 from data_utils import download_batch_subjects, get_raw_data
 import os
 
-personal_access_key_id = 'AKIAXO65CT57CGD7LRVW'
-secret_access_key = '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'
+personal_access_key_id = '' 
+secret_access_key = ''
 hcp_path = os.getcwd()
 #state_types = ["rest", "task_working_memory", "task_story_math", "task_motor"]
 get_raw_data(subject='105923', hcp_path=hcp_path, type_state="task_story_math")
